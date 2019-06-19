ce20f4f91396109484ba2d6361b5aae009691f0e
nestorovicevo <nestorovicevo@gmail.com>
2019-06-19 21:43:08 +0200

    post, get, i ostale metode u middleware-ima i rutiranje

--- app.js
+++ app.js
@@ -1,17 +1,20 @@
-const http = require('http');
-
 const express = require('express');
+
+const adminRoutes = require('./routes/admin');
+
+const shopRoutes = require('./routes/shop.js');
+
+const bodyParser = require('body-parser');
 
 const app = express();
 
-app.use((req, res, next) => {
-    console.log('In the middleware');
-    next(); // Alows the request to continue to the next middleware in the line
-});
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
 
 app.use((req, res, next) => {
-    console.log('In another middleware');
-    res.send('<h1>Hello from Express!</h1>');
+    res.status(404).send('<h1>Page not found!</h1>');
 });
 
 app.listen(3000);
