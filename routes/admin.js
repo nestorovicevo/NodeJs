ac944d7124fe8eb34aeb159c0fceb7f3547f9115
nestorovicevo <nestorovicevo@gmail.com>
2019-06-20 23:05:08 +0200

    Konvertovanje ostalih html stranica u pug

--- routes/admin.js
+++ routes/admin.js
@@ -10,7 +10,8 @@ const products = [];
 
 // /admin/add-product => GET
 router.get('/add-product', (req, res, next) => {
-  res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
+  // res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
+  res.render('add-product', {pageTitle: 'Add product'}); //render metod omogucava nam da si ovaj drugi parametar rendira u nasem view
 });
 
 // /admin/add-product => POST
