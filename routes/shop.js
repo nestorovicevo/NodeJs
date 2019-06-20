ddc07b6a1cf1cad418c5dd84ca64e2dc1c2e01ae
nestorovicevo <nestorovicevo@gmail.com>
2019-06-20 21:47:07 +0200

    Kreiranje pug fajla i rendiranje njega umesto obicnog html

--- routes/shop.js
+++ routes/shop.js
@@ -8,7 +8,8 @@ const adminData = require('./admin');
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-  res.render('shop');
+  const products = adminData.products;
+  res.render('shop', {prods: products, docTitle: 'Shop'}); //render metod omogucava nam da si ovaj drugi parametar rendira u nasem view
 });
 
 module.exports = router;
