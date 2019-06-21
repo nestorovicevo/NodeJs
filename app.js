29282d354c6e57d50c309ff8cb9396dfdf8c165e
nestorovicevo <nestorovicevo@gmail.com>
2019-06-21 21:52:21 +0200

    Krenula sa handlebars da radim, obrisala module i pokrenula npm install ponovo

--- app.js
+++ app.js
@@ -2,10 +2,15 @@ const path = require('path');
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const expressHbs = require('express-handlebars');
 
 const app = express();
 
-app.set('view engine', 'pug'); //ovom i linijom ispod govorimo da hocemo da kompajliramo pug engine i gde mozemo nadci ove templejte
+app.engine('hbs', expressHbs());
+app.set('view engine', 'hbs'); //ovom i linijom ispod govorimo da hocemo da kompajliramo pug engine i gde mozemo nadci ove templejte
+                                        //naknadno smo promenili pug u handlebars zato sto radimo sad sa drugim, i taj je za express
+                                        // i tu moze da stoji i npr handlebars, ali mora onda i gore i dole da pise handlebars, mora pisati identicno
+                                            //ali je bitno da onako kako nazovemo, to ce biti sufiks za ime fajla u views
 app.set('views', 'views'); //ova druga vrednost je ime foldera u kom se nalaze views, u ovom slucaju je ista
 
 const adminData = require('./routes/admin');
@@ -18,7 +23,7 @@ app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-    res.status(404).render('404', {pageTitle: 'Page not found'});
+    res.status(404).render('404', { pageTitle: 'Page Not Found' });
 });
 
 app.listen(3000);
