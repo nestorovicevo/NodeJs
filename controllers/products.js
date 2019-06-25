b1fb9504507ed1a06d19a8ef23f0f90adc0e39e4
nestorovicevo <nestorovicevo@gmail.com>
2019-06-25 19:38:28 +0200

    Refaktorisanje koda

--- controllers/products.js
+++ controllers/products.js
@@ -1,27 +1,30 @@
 const Product = require('../models/product');
 
 exports.getAddProduct = (req, res, next) => {
-    res.render('add-product',
-    {pageTitle: 'Add product',
+  res.render('add-product', {
+    pageTitle: 'Add Product',
     path: '/admin/add-product',
-    formsCSS: true, productCSS: true,
+    formsCSS: true,
+    productCSS: true,
     activeAddProduct: true
-   }); //render metod omogucava nam da si ovaj drugi parametar rendira u nasem view
-  };
+  });
+};
 
 exports.postAddProduct = (req, res, next) => {
-        const product = new Product(req.body.title);
-        product.save();
-    res.redirect('/');
-  }
+  const product = new Product(req.body.title);
+  product.save();
+  res.redirect('/');
+};
 
-  exports.getProducts = (req, res, next) => {
-      const products = Product.fetchAll();
+exports.getProducts = (req, res, next) => {
+  Product.fetchAll(products => {
     res.render('shop', {
       prods: products,
       pageTitle: 'Shop',
       path: '/',
-      hasProducts: products.lenght > 0,
-    activeShop: true,
-  productCSS: true}); //render metod omogucava nam da si ovaj drugi parametar rendira u nasem view
-  }
+      hasProducts: products.length > 0,
+      activeShop: true,
+      productCSS: true
+    });
+  });
+};
