904db88015bb91e4ddb0fc2f3a5989bbfd13da66
nestorovicevo <nestorovicevo@gmail.com>
2019-06-26 01:40:14 +0200

    Adding another item, also not finished

--- controllers/shop.js
+++ controllers/shop.js
@@ -27,6 +27,13 @@ exports.getCart = (req, res, next) => {
   });
 };
 
+exports.getOrders = (req, res, next) => {
+  res.render('shop/orders', {
+    path: '/Orders',
+    pageTitle: 'Your Orders'
+  });
+};
+
 exports.getCheckout = (req, res, next) => {
   res.render('shop/checkout', {
     path: '/checkout',
