6487819c057fb5193b98119f645a60156d7d71b1
nestorovicevo <nestorovicevo@gmail.com>
2019-06-26 18:07:28 +0200

    ispravka

--- models/cart.js
+++ models/cart.js
@@ -25,7 +25,7 @@ module.exports = class Cart {
       if (existingProduct) {
         updatedProduct = { ...existingProduct };
         updatedProduct.qty = updatedProduct.qty + 1;
-        cart.products = [...cart.products];
+        cart.products = [...cart.products]; ////ova linija ne mora
         cart.products[existingProductIndex] = updatedProduct;
       } else {
         updatedProduct = { id: id, qty: 1 };
