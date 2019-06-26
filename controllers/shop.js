442670ed80d2d5b9791955c8d60b8598df2d3f73
nestorovicevo <nestorovicevo@gmail.com>
2019-06-26 14:06:03 +0200

    Get product page by Id

--- controllers/shop.js
+++ controllers/shop.js
@@ -10,6 +10,12 @@ exports.getProducts = (req, res, next) => {
   });
 };
 
+exports.getProduct = (req, res, next) => {
+  const prodId = req.params.productId;  ///porductId mora da bude isto ime kao sto je u ruti navedeno
+  console.log(prodId);
+  res.redirect('/');
+}
+
 exports.getIndex = (req, res, next) => {
   Product.fetchAll(products => {
     res.render('shop/index', {
