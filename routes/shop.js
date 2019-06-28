5919c7de8adc0daf65037df2d642ed6084d6a84a
nestorovicevo <nestorovicevo@gmail.com>
2019-06-28 15:48:29 +0200

    Displaying Cart Items on the Cart

--- routes/shop.js
+++ routes/shop.js
@@ -14,6 +14,7 @@ router.get('/products/:productId', shopController.getProduct);
 
 router.post('/cart', shopController.postCart);
 
+router.get('/cart', shopController.getCart);
 
 router.get('/orders', shopController.getOrders);
 
