822197a2bf3df703ec7c101f625932fb29612be8
nestorovicevo <nestorovicevo@gmail.com>
2019-07-01 17:14:33 +0200

    Fetching a single product with the 'where' condition

--- models/product.js
+++ models/product.js
@@ -13,19 +13,18 @@ module.exports = class Product {
 
   save() {
     return db.execute(
-      'INSERT INTO products (title, price, description, imageUrl) VALUES (?, ?, ?, ?)',
-     [this.title, this.price, this.description, this.imageUrl]
-     );
+      'INSERT INTO products (title, price, imageUrl, description) VALUES (?, ?, ?, ?)',
+      [this.title, this.price, this.imageUrl, this.description]
+    );
   }
 
-  static deleteById(id) {
-
-  }
+  static deleteById(id) {}
 
   static fetchAll() {
     return db.execute('SELECT * FROM products');
   }
 
   static findById(id) {
+    return db.execute('SELECT * FROM products WHERE products.id = ?', [id]);
   }
 };
