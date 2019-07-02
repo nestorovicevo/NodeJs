f03132bf081514e1993abae217fc1a54aa8a3950
nestorovicevo <nestorovicevo@gmail.com>
2019-07-02 17:50:15 +0200

    Getting a single product

--- controllers/shop.js
+++ controllers/shop.js
@@ -14,19 +14,28 @@ exports.getProducts = (req, res, next) => {
     console.log(err);
   });
 };
-
 exports.getProduct = (req, res, next) => {
-  const prodId = req.params.productId;  // productId jer je isto tako u ruti navedeno
-  Product.findById(prodId)
-    .then(([product]) => {
+  const prodId = req.params.productId;
+  Product.findAll({ where: { id: prodId } })
+    .then(products => {
       res.render('shop/product-detail', {
-        product: product[0],
-        pageTitle: product.title,
+        product: products[0],
+        pageTitle: products[0].title,
         path: '/products'
       });
     })
-    .catch(err =>
-      console.log(err));
+    .catch(err => console.log(err));
+
+//     Product.findById(prodId)
+//     .then(product => {
+//       res.render('shop/product-detail', {
+//         product: product,
+//         pageTitle: product.title,
+//         path: '/products'
+//       });
+//     })
+//     .catch(err => console.log(err));
+// };                             //////////////OVAKO TREBA ALI NE RADI JER PISE D ANE POSTOJI FUNKCIJA findByID
 };
 
 exports.getIndex = (req, res, next) => {
