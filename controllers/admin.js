da8a8832222041be336e907d981892fc7852bf2f
nestorovicevo <nestorovicevo@gmail.com>
2019-07-02 18:07:31 +0200

    Updating/editing products

--- controllers/admin.js
+++ controllers/admin.js
@@ -34,7 +34,7 @@ exports.getEditProduct = (req, res, next) => {
     return res.redirect('/');
   }
   const prodId = req.params.productId;
-  Product.findById(prodId)
+  Product.findByPk(prodId)  ///findByPK je alternativa, jer nece da radi na findByID
     .then(product => {
       if (!product) {
         return res.redirect('/');
@@ -55,19 +55,21 @@ exports.postEditProduct = (req, res, next) => {
   const updatedPrice = req.body.price;
   const updatedImageUrl = req.body.imageUrl;
   const updatedDesc = req.body.description;
-  Product.findById(prodId)
-    .then(product => {
-      product.title = updatedTitle;
-      product.price = updatedPrice;
-      product.description = updatedDesc;
-      product.imageUrl = updatedImageUrl;
-      return product.save();
-    })
-    .then(result => {
-      console.log('UPDATED PRODUCT!');
-      res.redirect('/admin/products');
-    })
-    .catch(err => console.log(err));
+  Product.findByPk(prodId)
+  .then(product => {
+    product.title = updatedTitle;
+    product.price = updatedPrice;
+    product.description = updatedDesc;
+    product.imageUrl = updatedImageUrl;
+    return product.save();
+  })
+  .then(result => {
+    console.log('updated product');
+    res.redirect('/admin/products');
+  })
+  .catch(err => {
+    console.log(err)
+  })
 };
 
 exports.getProducts = (req, res, next) => {
