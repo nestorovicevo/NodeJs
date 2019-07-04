e9cbb993bf080535fe27f86833e149d057d49b22
nestorovicevo <nestorovicevo@gmail.com>
2019-07-04 13:13:25 +0200

    creating and menaging a dummy user

--- app.js
+++ app.js
@@ -23,6 +23,15 @@ const shopRoutes = require('./routes/shop');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use((req, res, next)=> {
+    User.findByPk(1)
+    .then( user=> {
+            req.user = user;
+            next();
+        })
+        .catch(err => console.log(err));
+});
+
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
@@ -33,12 +42,22 @@ User.hasMany(Product);
 
 
 sequelize
-.sync({ force: true })  // ovo automatski pravi od modela tabele u bazi!! ,, dok force: true adds a DROP TABLE IF EXISTS before trying to create the table - if you force, existing tables will be overwritten.
+// .sync({ force: true })  // ovo automatski pravi od modela tabele u bazi!! ,, dok force: true adds a DROP TABLE IF EXISTS before trying to create the table - if you force, existing tables will be overwritten.
+.sync()
 .then(result => {
+    return User.findByPk(1);
     // console.log(result);
-    app.listen(3000);
 })
-.catch(err => {
+.then(user => {
+    if (!user) {
+      return User.create({ name: 'Max', email: 'test@test.com' });
+    }
+    return user;
+  })
+  .then(user => {
+    // console.log(user);
+    app.listen(3000);
+  })
+  .catch(err => {
     console.log(err);
-});
-
+  });
